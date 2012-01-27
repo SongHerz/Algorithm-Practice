a4bca7fc971b3efbaec50423de9a828526a21776
dldz2002@gmail.com <song@bubble.(none)>
2012-01-27 20:06:05 +0800

    Change test architecture for monge matrix

--- Ch4/questions/test_monge_matrix_leftmost_min.cc
+++ Ch4/questions/test_monge_matrix_leftmost_min.cc
@@ -7,16 +7,67 @@ using namespace std;
 void print_matrix( ostream &os, const Matrix &m) {
 	for ( size_t i = 0; i < m.size(); ++i) {
 		vector< int> &cur_row = *m[ i];
-		for ( size_t j = 0; j < cur_row.size() - 1; ++j) {
-			os << setw(3) << cur_row[ j] << ", ";
+		os << "    ";
+		for ( size_t j = 0; j < cur_row.size(); ++j) {
+			os << setw(3) << cur_row[ j];
+			if ( j == cur_row.size() - 1) {
+				os << std::endl;
+			}
+			else {
+				os << ", ";
+			}
 		}
-		os << cur_row[ cur_row.size() - 1] << std::endl;
 	}
 }
 
 
+void print_matrix( ostream &os, const Matrix &m, const vector< size_t> min_col_idcs) {
+	for ( size_t i = 0; i < m.size(); ++i) {
+		vector< int> &cur_row = *m[ i];
+		os << "    ";
+		for ( size_t j = 0; j < cur_row.size(); ++j) {
+			if ( j == min_col_idcs[ i]) {
+				os << "(" << setw(2) << cur_row[ j] << ")";
+			}
+			else {
+				os << setw(4) << cur_row[ j];
+			}
+			if ( j == cur_row.size() - 1) {
+				os << std::endl;
+			}
+			else {
+				os << ", ";
+			}
+		}
+	}
+}
+
+void test_monge_matrix( const int *v, const size_t m, const size_t n) {
+	const Matrix *pm = create_matrix( v, m, n);
+	std::cout << "The matrix is:" << std::endl;
+	print_matrix( std::cout, *pm);
+
+	std::cout << "Is Monge Matrix ?" << std::endl;
+	std::cout << "    " << ( is_monge_matrix( *pm) ? "True" : "False") << std::endl;
+
+	vector< size_t> min_col_idcs( 7, 0);
+	monge_matrix_leftmost_min( *pm, min_col_idcs);
+
+	std::cout << "Mininum value index for each row are: " << std::endl;
+	std::cout << "    ";
+	for ( size_t i = 0; i < min_col_idcs.size(); ++i) {
+		std::cout << min_col_idcs[i] << ", ";
+	}
+	std::cout << std::endl;
+
+	std::cout << "Circle the mininums:" << std::endl;
+	print_matrix( std::cout, *pm, min_col_idcs);
+
+	free_matrix( pm);
+}
+
 int main() {
-	int v[ 7][ 5] = {
+	int v0[ 7][ 5] = {
 		{10, 17, 13, 28, 23},
 		{17, 22, 16, 29, 23},
 		{24, 28, 22, 34, 24},
@@ -26,18 +77,7 @@ int main() {
 		{75, 66, 51, 53, 34}
 	};
 
-	const Matrix *pm = create_matrix( reinterpret_cast<const int* const>(v), 7, 5);
-	print_matrix( std::cout, *pm);
-
-	std::cout << "Is Monge Matrix ? " << ( is_monge_matrix( *pm) ? "True" : "False") << std::endl;
-
-	vector< size_t> min_col_idcs( 7, 0);
-	monge_matrix_leftmost_min( *pm, min_col_idcs);
-
-	for ( size_t i = 0; i < min_col_idcs.size(); ++i) {
-		std::cout << min_col_idcs[i] << ", ";
-	}
-	std::cout << std::endl;
+	test_monge_matrix( reinterpret_cast< const int * const>( v0), 7, 5);
 
 	return 0;
 }
